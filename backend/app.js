4ce8599dee9fe4faecca2a8003d0222a73b08b99
Chen-Abudi <cheezumcali23@gmail.com>
2022-10-12 15:16:39 +0300

    added cors to app

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 
@@ -26,9 +27,12 @@ app.use(apiLimiter);
 //   };
 //   next();
 // });
-
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.options('*', cors());
+
 
 app.use(requestLogger);
 
