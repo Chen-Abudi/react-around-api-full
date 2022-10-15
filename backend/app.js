e22b48a9cdb211dd70178c04d2ede188f50079c5
Chen-Abudi <cheezumcali23@gmail.com>
2022-10-15 18:53:08 +0300

    added server crash testing

--- backend/app.js
+++ backend/app.js
@@ -31,6 +31,12 @@ app.options('*', cors());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
+
 app.use(router);
 
 app.use(errorLogger);
