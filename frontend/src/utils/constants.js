45222a8ec96a39731bafc2d4385aa437137a46c1
Chen-Abudi <cheezumcali23@gmail.com>
2022-10-15 22:29:54 +0300

    removed unneeded code

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -8,10 +8,5 @@ export const baseUrl = BASE_URL;
 export const headers = {
   Authorization: `Bearer ${localStorage.getItem("token")}`,
   "Content-Type": "application/json",
-
-  // export const baseUrl = "https://around.nomoreparties.co/v1/cohort-3-en";
-  // export const headers = {
-  //   authorization: "7206ed33-5b05-44a8-9280-ab5ebd7021d1",
-  //   "Content-Type": "application/json",
 };
 // ────────────────────────────────────────────────────────────────────────────
