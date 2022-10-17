f2ee7d0a28fac0992ec5d54b4f92cddc50494af8
Chen-Abudi <cheezumcali23@gmail.com>
2022-10-17 21:00:13 +0300

    modified Card like and remove buttons

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,10 +4,18 @@ import { CurrentUserContext } from "../contexts/CurrentUserContext";
 function Card({ card, likesCounter, onCardClick, onCardDelete, onCardLike }) {
   const currentUser = useContext(CurrentUserContext);
   // Checking if the current user is the owner of the current card
-  const isOwn = card.owner._id === currentUser._id;
+  // const isOwn = card.owner._id === currentUser._id;
+
+  const isOwn = card.owner === currentUser._id;
 
   // Check if the card was liked by the current user
-  const isLiked = card.likes.some((user) => user._id === currentUser._id);
+  // const isLiked = card.likes.some((user) => user._id === currentUser._id);
+
+  const isLiked = card.likes.some((user) => user === currentUser._id);
+
+  const postcardLikeButtonClassName = `postcard__like-button ${
+    isLiked && "postcard__like-button_active"
+  }`;
 
   function handleClick() {
     onCardClick(card);
@@ -23,14 +31,22 @@ function Card({ card, likesCounter, onCardClick, onCardDelete, onCardLike }) {
 
   return (
     <li className="postcard" key={card._id}>
-      <button
+      {isOwn && (
+        <button
+          className="postcard__remove-button"
+          aria-label="remove postcard"
+          type="button"
+          onClick={handleCardDeleteClick}
+        />
+      )}
+      {/* <button
         className={`postcard__remove-button ${
           isOwn && "postcard__remove-button_visible"
         }`}
         aria-label="remove postcard"
         type="button"
         onClick={handleCardDeleteClick}
-      />
+      /> */}
       <img
         className="postcard__image"
         src={card.link}
@@ -41,9 +57,7 @@ function Card({ card, likesCounter, onCardClick, onCardDelete, onCardLike }) {
         <h2 className="postcard__title">{card.name}</h2>
         <div className="postcard__like-container">
           <button
-            className={`postcard__like-button ${
-              isLiked && "postcard__like-button_active"
-            }`}
+            className={postcardLikeButtonClassName}
             aria-label="like-or-unlike-postcard"
             type="button"
             onClick={handleLikeClick}
