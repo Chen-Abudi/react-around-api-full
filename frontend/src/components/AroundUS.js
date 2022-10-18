f2a1cf8609feb64e3e6eef2044e23a613768c89f
Chen-Abudi <cheezumcali23@gmail.com>
2022-10-18 18:24:00 +0300

    modifies cards like

--- frontend/src/components/AroundUS.js
+++ frontend/src/components/AroundUS.js
@@ -74,16 +74,26 @@ function AroundUS() {
 
   function handleCardLike(card) {
     // Check one more time if this card was already liked
-    const isLiked = card.likes.some((id) => id === currentUser._id);
+    const isLiked = card.likes.some((cardId) => cardId === currentUser._id);
     // Send a request to the API and getting the updated card data
+    // api
+    //   .cardLike(card._id, isLiked)
+    //   .then((newCard) => {
+    //     const newCards = cards.map((currentCard) =>
+    //       currentCard._id === card._id ? newCard : currentCard
+    //     );
+    //     setCards(newCards.data);
+    //   })
+    //   .catch((err) => console.log(err));
     api
       .cardLike(card._id, isLiked)
-      .then((newCard) => {
-        const newCards = cards.map((currentCard) =>
-          currentCard._id === card._id ? newCard : currentCard
-        );
-        setCards(newCards.data);
-      })
+      .then((newCard) =>
+        setCards((state) =>
+          state.map((currentCard) =>
+            currentCard._id === card._id ? newCard.data : currentCard
+          )
+        )
+      )
       .catch((err) => console.log(err));
   }
 
