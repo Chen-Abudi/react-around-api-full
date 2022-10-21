ba41426495f8b11e6afa939cc7a30510fd7d58a8
Chen-Abudi <cheezumcali23@gmail.com>
2022-10-21 21:28:52 +0300

    node env

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const User = require('../models/user');
 
