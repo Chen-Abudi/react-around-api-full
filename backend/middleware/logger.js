33b41db26ec638c81659a16c457590c17447eab7
Chen-Abudi <cheezumcali23@gmail.com>
2023-03-27 14:47:23 +0300

    fix: logger

--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -1,21 +1,53 @@
+// const winston = require('winston');
+// const expressWinston = require('express-winston');
+
+// const fs = require('fs');
+
+// const logDir = './logs';
+
+// if (!fs.existsSync(logDir)) {
+//   fs.mkdirSync(logDir);
+// }
+
+// const requestLogger = expressWinston.logger({
+//   transports: [new winston.transports.File({ filename: './logs/request.log' })],
+//   format: winston.format.json(),
+// });
+
+// const errorLogger = expressWinston.errorLogger({
+//   transports: [new winston.transports.File({ filename: './logs/error.log' })],
+//   format: winston.format.json(),
+// });
+
+// module.exports = {
+//   requestLogger,
+//   errorLogger,
+// };
+
 const winston = require('winston');
 const expressWinston = require('express-winston');
-
 const fs = require('fs');
+const path = require('path');
 
-const logDir = './logs';
+const logsDir = path.join(process.cwd(), 'data', 'logs');
 
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
+if (!fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir, { recursive: true });
 }
 
 const requestLogger = expressWinston.logger({
-  transports: [new winston.transports.File({ filename: './logs/request.log' })],
+  transports: [
+    new winston.transports.File({
+      filename: path.join(logsDir, 'request.log'),
+    }),
+  ],
   format: winston.format.json(),
 });
 
 const errorLogger = expressWinston.errorLogger({
-  transports: [new winston.transports.File({ filename: './logs/error.log' })],
+  transports: [
+    new winston.transports.File({ filename: path.join(logsDir, 'error.log') }),
+  ],
   format: winston.format.json(),
 });
 
