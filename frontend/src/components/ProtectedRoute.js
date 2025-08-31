a37aa3336f8771870c4d3ca0c94dc80135f09c02
Grace Chen Abudi <cheezumcali23@gmail.com>
2025-08-31 20:19:42 +0300

    chore: Clear comments

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -10,25 +10,3 @@ const ProtectedRoute = ({ children }) => {
 };
 
 export default ProtectedRoute;
-
-// import React, { useContext } from "react";
-// import { Route, Navigate } from "react-router-dom";
-// import { AccountContext } from "../contexts/AccountContext";
-
-// const ProtectedRoute = ({ component: Component, ...props }) => {
-//   const user = useContext(AccountContext);
-
-//   return (
-//     <Route>
-//       {() =>
-//         user.loggedIn === true ? (
-//           <Component {...props} />
-//         ) : (
-//           <Navigate to="/signin" />
-//         )
-//       }
-//     </Route>
-//   );
-// };
-
-// export default ProtectedRoute;
